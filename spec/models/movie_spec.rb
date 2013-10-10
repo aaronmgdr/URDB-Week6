0b584ef9ca1a369b8113577f461e49cbe671ce81
MakerSquare Student <students@themakersquare.com>
2013-10-10 10:03:33 -0500

    write passing test for #snippet

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -1,5 +1,28 @@
 require 'spec_helper'
 
 describe Movie do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:movie) { Movie.new }
+
+  describe "#snippet" do
+    context "when description is less than or equal to 50 characters" do
+      it "returns the full description" do
+
+        allow(movie).to receive(:description).and_return("abcdefghijklmnopqrstuvwxyabcdefghijklmnopqrstuvwxy")
+
+        expect(movie.snippet).to eq('abcdefghijklmnopqrstuvwxyabcdefghijklmnopqrstuvwxy')
+
+      end
+    end
+
+    context "when description is greater than 50 characters" do
+      it "returns the first 47 characters and appends '...'" do
+
+        allow(movie).to receive(:description).and_return("abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz")
+
+        expect(movie.snippet).to eq('abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstu...')
+
+      end
+    end
+
+  end
 end
