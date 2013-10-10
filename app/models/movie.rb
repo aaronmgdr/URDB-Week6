632b05af5a676b57a36a63578c7bb722f8ae9916
MakerSquare Student <students@themakersquare.com>
2013-10-10 10:14:51 -0500

    write failing test for #audience_rating"

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -6,8 +6,8 @@ class Movie < ActiveRecord::Base
   def rotten_finder
     RottenMovie.find(title: title, limit: 1)
   end
-  
+
   def snippet
-    description.truncate 50
+    description.to_s.truncate 50
   end
 end
--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe Movie do
   let(:movie) { Movie.new }
+  let(:rotten_finder_return) { OpenStruct.new(ratings: OpenStruct.new(audience_score: 84)) }
 
   describe "#snippet" do
     context "when description is less than or equal to 50 characters" do
@@ -24,5 +25,31 @@ describe Movie do
       end
     end
 
+    context "when description is nil" do
+      it "returns an empty string" do
+
+        allow(movie).to receive(:description).and_return(nil)
+
+        expect(movie.snippet).to eq('')
+      end
+    end
+  end
+
+  describe "#audience_rating" do
+    context "when Movie is found on Rotten Tomatoes" do
+      it "returns the audience score for the Movie" do
+        allow(movie).to receive(:rotten_finder).and_return(rotten_finder_return)
+
+        expect(movie.audience_rating).to eq(84)
+      end
+    end
+
+    context "when Movie is not found on Rotten Tomatoes" do
+      it "returns nil" do
+        allow(movie).to receive(:rotten_finder).and_return(rotten_finder_return)
+
+        expect(movie.audience_rating).to eq(nil)
+      end
+    end
   end
 end
